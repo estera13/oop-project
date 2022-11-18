d29e3ca4af9575958f437eb033f78570dca5aaf8
estera13 <117632886+estera13@users.noreply.github.com>
2022-11-18 21:17:53 +0200

    Update Film.cpp

--- Film.cpp
+++ Film.cpp
@@ -1,1 +1,24 @@
 #include "Film.h"
+using namespace std;
+
+
+Film::Film(const Film &copie)
+{
+    id=copie.id;
+    nume=copie.nume;
+    gen=copie.gen;
+    ora=copie.ora;
+    durata=copie.durata;
+    pret=copie.pret;
+}
+
+Film&Film::operator=(const Film& other){
+    id = other.id;
+    nume = other.nume;
+    gen = other.gen;
+    ora = other.ora;
+    durata = other.durata;
+    pret= other.pret;
+    return *this;
+}
+Film::~Film(){}
