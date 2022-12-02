21cd34edf661abf073671441fdf2cf71eebc3389
estera13 <117632886+estera13@users.noreply.github.com>
2022-12-02 23:11:13 +0200

    Update Client.cpp

--- Client.cpp
+++ Client.cpp
@@ -5,11 +5,13 @@ using namespace std;
 Client::Client(const Client &copie)
 {
     nume=copie.nume;
+    prenume=copie.prenume;
     varsta=copie.varsta;
 }
 
 Client&Client::operator=(const Client& other){
     nume = other.nume;
+    prenume=other.prenume;
     varsta = other.varsta;
     return *this;
 }
