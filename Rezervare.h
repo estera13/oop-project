3c63d3896539baf355cb21ca74acab0ffc6d3194
estera13 <117632886+estera13@users.noreply.github.com>
2022-12-18 20:28:40 +0200

    Update Rezervare.h

--- Rezervare.h
+++ Rezervare.h
@@ -1,75 +1,96 @@
+
 #ifndef OOP_REZERVARE_H
 #define OOP_REZERVARE_H
 
 #include <ostream>
 #include "Film.h"
 #include "Client.h"
-
 class Rezervare{
-    const int id;
-    int numar_loc;
-    int numar_rand;
+     int numar_loc;
+     int numar_rand;
+    float pret;
     Client client;
     Film film;
 public:
 
     Rezervare();
-    Rezervare(const int id, const int &numar_loc, const int &numar_rand, const Client &client, const Film &film) : id(id), numar_loc(numar_loc), numar_rand(numar_rand), client(client),film (film) {};
-    ~Rezervare() {};
-
-    friend std::ostream &operator<<(std::ostream &os, const Rezervare &Rezervare) {
-        os << "id: " << Rezervare.id << " numar_loc: " << Rezervare.numar_loc << " numar_rand: " << Rezervare.numar_rand
-        << " client: " << Rezervare.client << " film: " << Rezervare.film <<"\n";
-        return os;
-    }
-
-    void reducere()
-    {
-        if (client.getVarsta()<=12)
-        {
-            float p=film.getPret()-film.getPret()*25/100;
-            film.setPret(p);
-            std::cout<<film.getPret()<<" lei"<<'\n';
-        }
-    };
-
-    void ochelari3D()
-    {
-        if(film.getTehnologie()=="3D")
-        {
-            float p=film.getPret()+5;
-            film.setPret(p);
-            std::cout<<film.getPret()<<" lei"<<'\n';
-        }
-    }
-
-    void restrictieVarsta()
-    {
-        if(client.getVarsta()<film.getPegi())
-        {
-            std::cout<<"Eroare varsta"<<'\n';
-        }
-    }
-
-
-    int getNumar_loc()
-    {
-        return numar_loc;
-    }
-    void setNumar_loc(int numar_loc0)
-    {
-        numar_loc=numar_loc0;
-    }
-
-    int getNumar_rand()
-    {
-        return numar_rand;
-    }
-    void setNumar_rand(int numar_rand0)
-    {
-        numar_rand=numar_rand0;
-    }
-
+    Rezervare(  int &numar_loc,  int &numar_rand, float pret, Client client, Film film);
+    ~Rezervare();
+    Rezervare(const Rezervare &);
+    Rezervare& operator=(const Rezervare&);
+    friend std::ostream &operator<<(std::ostream &os, const Rezervare &Rezervare);
+     void reducere();
+     void ochelari3D();
+     float getPret();
+     void setPret(int pret0);
+     int getNumar_loc();
+     void setNumar_loc(int numar_loc0);
+     int getNumar_rand();
+     void setNumar_rand(int numar_rand0);
 };
+Rezervare::Rezervare( int &numar_loc,  int &numar_rand, float pret, Client client, Film film) : numar_loc(numar_loc), numar_rand(numar_rand), pret(pret), client(client), film(film)
+{
+    if(film.getPegi()==18 && client.getVarsta()<film.getPegi())
+    {
+        throw(eroare_varstaPegi());
+    }
+}
 
+Rezervare&Rezervare::operator=(const Rezervare& other){
+    numar_loc=other.numar_loc;
+    numar_rand=other.numar_rand;
+    pret=other.pret;
+    client=other.client;
+    film=other.film;
+    return *this;
+}
+
+Rezervare::~Rezervare() {}
+
+std::ostream &operator<<(std::ostream &os, const Rezervare &Rezervare) {
+    os  << " numar_loc: " << Rezervare.numar_loc << " numar_rand: " << Rezervare.numar_rand << " pret: " <<Rezervare.pret <<" lei\n"
+        << " Date client: " <<Rezervare.client << " Date film: " << Rezervare.film <<"\n";
+    return os;
+}
+
+void Rezervare::reducere() {
+    if (client.getVarsta()<=12)
+    {
+        float p=film.getPret()-film.getPret()*25/100;
+        pret=p;
+    }
+}
+
+void Rezervare::ochelari3D() {
+    if(film.getTehnologie()=="3D")
+    {
+        float p=film.getPret()+5;
+        pret=p;
+    }
+    else pret=film.getPret();
+}
+
+float Rezervare::getPret() {
+    return pret;
+}
+
+void Rezervare::setPret(int pret0) {
+    pret=pret0;
+}
+
+int Rezervare::getNumar_loc() {
+    return numar_loc;
+}
+
+void Rezervare::setNumar_loc(int numar_loc0) {
+    numar_loc=numar_loc0;
+}
+
+int Rezervare::getNumar_rand() {
+    return numar_rand;
+}
+
+void Rezervare::setNumar_rand(int numar_rand0) {
+    numar_rand=numar_rand0;
+};
 #endif //OOP_REZERVARE_H
