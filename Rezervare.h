226ad51ac31ef37f1fc00050befd64ef6c7eb7ac
estera13 <117632886+estera13@users.noreply.github.com>
2022-12-18 19:54:20 +0200

    Update Rezervare.h

--- Rezervare.h
+++ Rezervare.h
@@ -4,28 +4,74 @@
 #include <ostream>
 #include "Film.h"
 #include "Client.h"
+
 class Rezervare{
-     int numar_loc;
-     int numar_rand;
-    float pret;
+    const int id;
+    int numar_loc;
+    int numar_rand;
     Client client;
     Film film;
 public:
 
     Rezervare();
-    Rezervare(  int &numar_loc,  int &numar_rand, float pret, Client client, Film film);
-    ~Rezervare();
+    Rezervare(const int id, const int &numar_loc, const int &numar_rand, const Client &client, const Film &film) : id(id), numar_loc(numar_loc), numar_rand(numar_rand), client(client),film (film) {};
+    ~Rezervare() {};
     Rezervare(const Rezervare &);
     Rezervare& operator=(const Rezervare&);
-    friend std::ostream &operator<<(std::ostream &os, const Rezervare &Rezervare);
-     void reducere();
-     void ochelari3D();
-     float getPret();
-     void setPret(int pret0);
-     int getNumar_loc();
-     void setNumar_loc(int numar_loc0);
-     int getNumar_rand();
-     void setNumar_rand(int numar_rand0);
+
+    friend std::ostream &operator<<(std::ostream &os, const Rezervare &Rezervare) {
+        os << "id: " << Rezervare.id << " numar_loc: " << Rezervare.numar_loc << " numar_rand: " << Rezervare.numar_rand
+        << " client: " << Rezervare.client << " film: " << Rezervare.film <<"\n";
+        return os;
+    }
+
+    void reducere()
+    {
+        if (client.getVarsta()<=12)
+        {
+            float p=film.getPret()-film.getPret()*25/100;
+            film.setPret(p);
+            std::cout<<film.getPret()<<" lei"<<'\n';
+        }
+    };
+
+    void ochelari3D()
+    {
+        if(film.getTehnologie()=="3D")
+        {
+            float p=film.getPret()+5;
+            film.setPret(p);
+            std::cout<<film.getPret()<<" lei"<<'\n';
+        }
+    }
+
+    void restrictieVarsta()
+    {
+        if(client.getVarsta()<film.getPegi())
+        {
+            std::cout<<"Eroare varsta"<<'\n';
+        }
+    }
+
+
+    int getNumar_loc()
+    {
+        return numar_loc;
+    }
+    void setNumar_loc(int numar_loc0)
+    {
+        numar_loc=numar_loc0;
+    }
+
+    int getNumar_rand()
+    {
+        return numar_rand;
+    }
+    void setNumar_rand(int numar_rand0)
+    {
+        numar_rand=numar_rand0;
+    }
+
 };
 
 #endif //OOP_REZERVARE_H
