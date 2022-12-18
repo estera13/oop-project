184b8922620c7f2dcd58c56a4e1202dde9e298f9
estera13 <117632886+estera13@users.noreply.github.com>
2022-12-18 19:54:50 +0200

    Update Rezervare.cpp

--- Rezervare.cpp
+++ Rezervare.cpp
@@ -1,63 +1,24 @@
 #include "Rezervare.h"
 
+using namespace std;
 
-Rezervare::Rezervare( int &numar_loc,  int &numar_rand, float pret, Client client, Film film) : numar_loc(numar_loc), numar_rand(numar_rand), pret(pret), client(client), film(film)
 
+Rezervare::Rezervare(const Rezervare &copie)
+{
+    id=copie.id;
+    numar_loc=copie.numar_loc;
+    numar_rand=copie.numar_rand;
+    client=copie.client;
+    film=copie.film;
+}
 
 Rezervare&Rezervare::operator=(const Rezervare& other){
+    id=other.id;
     numar_loc=other.numar_loc;
     numar_rand=other.numar_rand;
-    pret=other.pret;
     client=other.client;
     film=other.film;
     return *this;
 }
+Rezervare::~Rezervare(){}
 
-Rezervare::~Rezervare() {}
-
-std::ostream &operator<<(std::ostream &os, const Rezervare &Rezervare) {
-    os  << " numar_loc: " << Rezervare.numar_loc << " numar_rand: " << Rezervare.numar_rand << " pret: " <<Rezervare.pret <<" lei\n"
-        << " Date client: " <<Rezervare.client << " Date film: " << Rezervare.film <<"\n";
-    return os;
-}
-
-void Rezervare::reducere() {
-    if (client.getVarsta()<=12)
-    {
-        float p=film.getPret()-film.getPret()*25/100;
-        pret=p;
-    }
-}
-
-void Rezervare::ochelari3D() {
-    if(film.getTehnologie()=="3D")
-    {
-        float p=film.getPret()+5;
-        pret=p;
-    }
-    else pret=film.getPret();
-}
-
-float Rezervare::getPret() {
-    return pret;
-}
-
-void Rezervare::setPret(int pret0) {
-    pret=pret0;
-}
-
-int Rezervare::getNumar_loc() {
-    return numar_loc;
-}
-
-void Rezervare::setNumar_loc(int numar_loc0) {
-    numar_loc=numar_loc0;
-}
-
-int Rezervare::getNumar_rand() {
-    return numar_rand;
-}
-
-void Rezervare::setNumar_rand(int numar_rand0) {
-    numar_rand=numar_rand0;
-};
