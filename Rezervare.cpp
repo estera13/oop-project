d9de1a862198512d588a809b00a991342cd95829
estera13 <117632886+estera13@users.noreply.github.com>
2022-12-18 20:09:27 +0200

    Update Rezervare.cpp

--- Rezervare.cpp
+++ Rezervare.cpp
@@ -3,17 +3,7 @@
 using namespace std;
 
 
-Rezervare::Rezervare(const Rezervare &copie)
-{
-    id=copie.id;
-    numar_loc=copie.numar_loc;
-    numar_rand=copie.numar_rand;
-    client=copie.client;
-    film=copie.film;
-}
-
 Rezervare&Rezervare::operator=(const Rezervare& other){
-    id=other.id;
     numar_loc=other.numar_loc;
     numar_rand=other.numar_rand;
     client=other.client;
