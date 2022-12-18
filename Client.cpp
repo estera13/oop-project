b94445afb72eaa1a6cab3dd2a53eced18f320fc1
estera13 <117632886+estera13@users.noreply.github.com>
2022-12-18 19:52:12 +0200

    Update Client.cpp

--- Client.cpp
+++ Client.cpp
@@ -1,7 +1,6 @@
 #include "Client.h"
-#include <iostream>
+using namespace std;
 
-Client::Client(const std::string &nume, const std::string &prenume, int varsta) : nume(nume), prenume(prenume), varsta(varsta) 
 
 Client::Client(const Client &copie)
 {
@@ -10,39 +9,14 @@ Client::Client(const Client &copie)
     varsta=copie.varsta;
 }
 
-Client& Client::operator=(const Client& other){
+Client&Client::operator=(const Client& other){
     nume = other.nume;
     prenume=other.prenume;
     varsta = other.varsta;
     return *this;
 }
-std::ostream &operator<<(std::ostream &os, const Client &client) {
-    os << "nume: " << client.nume <<" prenume: "<< client.prenume << " varsta: " << client.varsta << " ani " <<"\n";
-    return os;
-};
+Client::~Client(){}
 
-int Client::getVarsta() {
-    return varsta;
-}
-Client::~Client() {}
 
-void Client::setVarsta(int varsta0) {
-    varsta=varsta0;
-}
 
-void Client::setPrenume(const std::string &prenume0) {
-    prenume = prenume0;
-}
-
-const std::string &Client::getPrenume() {
-    return prenume;
-}
-
-void Client::setNume(const std::string &nume0) {
-    nume = nume0;
-}
-
-const std::string &Client::getNume() {
-    return nume;
-}
 
