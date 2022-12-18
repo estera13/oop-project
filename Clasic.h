405dabdf9be6a1e567258485625d2896407a2b0e
estera13 <117632886+estera13@users.noreply.github.com>
2022-12-19 00:44:10 +0200

    Update Clasic.h

--- Clasic.h
+++ Clasic.h
@@ -6,7 +6,7 @@
 #include "Film.h"
 
 class Clasic : Film{
-    int an;
+    protected: int an;
 public:
     Clasic(const std::string &nume, const std::string &gen, const std::string &ora, int durata, float pret,
            const std::string &tehnologie, int pegi, int an) : Film(nume, gen, ora, durata, pret,tehnologie, pegi), an(an) {}
