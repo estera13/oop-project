472bc4675659ac48da7584e9eb17642efe89f362
estera13 <117632886+estera13@users.noreply.github.com>
2022-12-19 00:49:17 +0200

    Update Comedie.h

--- Comedie.h
+++ Comedie.h
@@ -11,7 +11,7 @@ class Comedie : Film{
    std::string actor_principal;
 public:
     Comedie(const std::string &nume, const std::string &gen, const std::string &ora, int durata, float pret,
-           const std::string &tehnologie, int pegi, const std::string actor_principal) : Film(nume, gen, ora, durata, pret,tehnologie, pegi), actor_principal(actor_principal) {}
+           const std::string &tehnologie, int pegi, const std::string &actor_principal) : Film(nume, gen, ora, durata, pret,tehnologie, pegi), actor_principal(actor_principal) {}
     ~Comedie() {}
 };
 
