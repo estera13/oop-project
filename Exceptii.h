04e540cc057c79a56e941aa50db854711d981895
estera13 <117632886+estera13@users.noreply.github.com>
2022-12-18 21:16:52 +0200

    Update Exceptii.h

--- Exceptii.h
+++ Exceptii.h
@@ -9,18 +9,18 @@
 class eroare : public std::runtime_error {
 public:
     eroare(const std::string &arg) : runtime_error("Eroare: " + arg) {};
-};
+}
 
 
 class eroare_varstaPegi : public eroare {
 public:
     eroare_varstaPegi() : eroare(" Filmul nu este recomandat persoanelor sub 18 ani!") {};
-};
+}
 
 class eroare_varsta : public eroare {
 public:
     eroare_varsta() : eroare(" Varsta nu poate fi negativa!") {};
-};
+}
 
 
 #endif //OOP_EXCEPTII_H
