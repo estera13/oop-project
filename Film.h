c640f73cdd0cdfa12f7724d859ffe527c651958e
estera13 <117632886+estera13@users.noreply.github.com>
2022-12-18 19:53:45 +0200

    Update Film.h

--- Film.h
+++ Film.h
@@ -13,24 +13,69 @@ class Film{
     int pegi;
 public:
 
-    Film(const std::string &nume, const std::string &gen, const std::string &ora, int durata, float pret, const std::string &tehnologie, int pegi) ;
-    ~Film();
+    Film();
+    Film(const std::string &nume, const std::string &gen, const std::string &ora, int durata, float pret, const std::string &tehnologie, int pegi) : nume(nume), gen(gen), ora(ora), durata(durata), pret(pret) ,tehnologie(tehnologie), pegi(pegi) {};
+    ~Film() {};
     Film(const Film &);
     Film& operator=(const Film&);
-    friend std::ostream &operator<<(std::ostream &os, const Film &film);
-    const std::string &getNume ();
-    void setNume(const std::string &nume0);
-    const std::string &getGen();
-    void setGen(const std::string &gen0);
-    const std::string &getOra();
-    void setOra(const std::string &ora0);
-    int getDurata();
-    void setDurata(int durata0);
-    float getPret();
-    void setPret(int pret0);
-    const std::string &getTehnologie();
-    int getPegi();
-    void setPegi(int pegi0);
+    friend std::ostream &operator<<(std::ostream &os, const Film &film)
+    {
+        os << " nume: " << film.nume << " gen: " << film.gen << " ora: "
+           << film.ora << " durata: " << film.durata << " minute" << " pret: " << film.pret<< " lei " << " tehnologie: " << film.tehnologie << " PEGI: " << film.pegi << "\n";
+        return os;
+    }
+    const std::string &getNume()
+    {
+        return nume;
+    }
+    void setNume(const std::string &nume0)
+    {
+        nume = nume0;
+    }
+
+    const std::string &getGen()
+    {
+        return gen;
+    }
+    void setGen(const std::string &gen0)
+    {
+        gen = gen0;
+    }
+
+    const std::string &getOra()
+    {
+        return ora;
+    }
+    void setOra(const std::string &ora0)
+    {
+        ora = ora0;
+    }
+
+    int getDurata()
+    {
+        return durata;
+    }
+    void setDurata(int durata0)
+    {
+        durata=durata0;
+    }
+
+    float getPret()
+    {
+        return pret;
+    }
+    void setPret(int pret0)
+    {
+        pret=pret0;
+    }
+    const std::string &getTehnologie()
+    {
+        return tehnologie;
+    }
+    int getPegi()
+    {
+        return pegi;
+    }
 };
 
 
